b563b0702cb9dea663f0e55345dff44f2e6ebf5c
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-07-07 13:23:49 -0700

    Update scscj.R

--- scscj.R
+++ scscj.R
@@ -1,18 +1,24 @@
 #Jaccard scraper for scopus
 scscj<-function(apikey,query1,query2){
-  list1<-c()
+  listOR<-c()
   for(i in 1:length(query1)){
-    list1<-c(list1,as.numeric(xpathApply(htmlTreeParse(getURL(paste0("http://api.elsevier.com/content/search/scopus/?apiKey=",apikey,"&query=",query1[[i]],"&field=doi&httpAccept=%20application%2Fatom%2Bxml&count=1&start=0")),
-      useInternalNode=T),"//totalresults",xmlValue)))
+    for(j in 1:length(query2)){
+      listOR<-c(listOR,as.numeric(xpathApply(htmlTreeParse(getURL(paste0("http://api.elsevier.com/content/search/scopus/?apiKey=",apikey,"&query=",query1[[i]]"+OR+",qu,"&field=doi&httpAccept=%20application%2Fatom%2Bxml&count=1&start=0")),
+        useInternalNode=T),"//totalresults",xmlValue)))
+    }
   }
-  list1
 #
-  list2<-c()
+  listAND<-c()
   for(i in 1:length(query2)){
     list2<-c(list2,as.numeric(xpathApply(htmlTreeParse(getURL(paste0("http://api.elsevier.com/content/search/scopus/?apiKey=",apikey,"&query=",query2[[i]],"&field=doi&httpAccept=%20application%2Fatom%2Bxml&count=1&start=0")),
       useInternalNode=T),"//totalresults",xmlValue)))
   }
-#
-
 
 }
+
+
+#Example: Jaccard search of dynamic light scattering versus its synonyms.
+syn<-unlist(strsplit(unlist(xpathApply(CMO.owl,"//class/label[.='dynamic light scattering']/ancestor::class/hasexactsynonym",xmlValue)),
+      "\n +"))
+query2<-paste0("%22",gsub(" ","+",syn),"%22")
+query1<-"%22dynamic+light+scattering%22"

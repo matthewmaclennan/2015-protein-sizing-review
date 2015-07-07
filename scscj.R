8196fbf28d3c65005a94b58c875f3d8b9323b00f
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-07-07 14:03:42 -0700

    Update scscj.R

--- scscj.R
+++ scscj.R
@@ -22,5 +22,27 @@ syn<-unlist(strsplit(unlist(xpathApply(CMO.owl,"//class/label[.='dynamic light s
       "\n +"))
 query2<-paste0("%22",gsub(" ","+",syn),"%22")
 query1<-"%22dynamic+light+scattering%22"
+
+
+scscj<-function(apikey,query1,query2){
 q1and2<-apply(as.matrix(expand.grid(query1,query2)),1,function(x) paste0(x,collapse="+AND+"))
 q1or2<-apply(as.matrix(expand.grid(query1,query2)),1,function(x) paste0(x,collapse="+OR+"))
+apikey<-as.character(apikey)
+c1and2<-c()
+for(i in 1:length(q1and2)){
+c1and2<-c(c1and2,as.numeric(xpathApply(htmlTreeParse(getURL(paste0("http://api.elsevier.com/content/search/scopus/?apiKey=",
+apikey,
+"&query=",
+q1and2,
+"&field=doi&httpAccept=%20application%2Fatom%2Bxml&count=1&start=0")),
+        useInternalNode=T),"//totalresults",xmlValue)))
+}
+c1and2
+
+c1or2<-c()
+for(i in 1:length(q1or2)){
+c1or2<-c(c1or2,as.numeric(xpathApply(htmlTreeParse(getURL(paste0("http://api.elsevier.com/content/search/scopus/?apiKey=",apikey,"&query=",q1or2,"&field=doi&httpAccept=%20application%2Fatom%2Bxml&count=1&start=0")),
+        useInternalNode=T),"//totalresults",xmlValue)))
+}
+cmatrix<-cbind(as.matrix(q1and2),as.matrix(c1and2),as.matrix(q1or2),as.matrix(c1or2))
+}

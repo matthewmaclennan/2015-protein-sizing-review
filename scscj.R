3b68b836dd8f40c1fd6846661e463d10020575d7
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-07-07 14:44:37 -0700

    Update scscj.R
    Added the loop index [[i]] to fix the entire loop. Added "cmatrix" declaration at the end of function in order to create the object.

--- scscj.R
+++ scscj.R
@@ -22,19 +22,19 @@ syn<-unlist(strsplit(unlist(xpathApply(CMO.owl,"//class/label[.='dynamic light s
       "\n +"))
 query2<-paste0("%22",gsub(" ","+",syn),"%22")
 query1<-"%22dynamic+light+scattering%22"
-
+<><><><><><><><><><><><><><><><><><><><>
 
 scscj<-function(apikey,query1,query2){
 q1and2<-apply(as.matrix(expand.grid(query1,query2)),1,function(x) paste0(x,collapse="+AND+"))
 q1or2<-apply(as.matrix(expand.grid(query1,query2)),1,function(x) paste0(x,collapse="+OR+"))
 apikey<-as.character(apikey)
-c1and2<-c()
 
+c1and2<-c()
 for(i in 1:length(q1and2)){
 c1and2<-c(c1and2,as.numeric(xpathApply(htmlTreeParse(getURL(paste0("http://api.elsevier.com/content/search/scopus/?apiKey=",
 apikey,
 "&query=",
-q1and2,
+q1and2[[i]],
 "&field=doi&httpAccept=%20application%2Fatom%2Bxml&count=1&start=0")),
         useInternalNode=T),"//totalresults",xmlValue)))
 }
@@ -45,7 +45,7 @@ for(i in 1:length(q1or2)){
   c1or2<-c(c1or2,as.numeric(xpathApply(htmlTreeParse(getURL(paste0("http://api.elsevier.com/content/search/scopus/?apiKey=",
   apikey,
   "&query=",
-  q1or2,
+  q1or2[[i]],
   "&field=doi&httpAccept=%20application%2Fatom%2Bxml&count=1&start=0")),
         useInternalNode=T),"//totalresults",xmlValue)))
 }
@@ -53,4 +53,5 @@ c1or2
 
 cmatrix<-cbind(q1and2,c1and2,q1or2,c1or2)
 colnames(cmatrix)<-c("AND search","1 and 2","OR search","1 or 2")
+cmatrix
 }
